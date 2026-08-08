b9ffe2a414b36131c89599e9e6429e762b047d91
starrobe <starrobe@163.com>
2026-08-09 00:40:28 +0800

    chore: remove debug code

--- src/terminal/useTerminal.ts
+++ src/terminal/useTerminal.ts
@@ -229,7 +229,6 @@ export function useTerminal() {
 
     xtermRef.current = term;
     fitAddonRef.current = fitAddon;
-    (window as any).__term = term;
 
     // Handle input
     term.onData((data) => {
